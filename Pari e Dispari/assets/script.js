1320a6ae1a5c8f3b0c8a9099841ed0c2ba5f53d5
AndresAco92 <andres.antonio.aco92@gmail.com>
2021-04-15 14:46:52 +0200

    ESercizio corretto

--- Pari e Dispari/assets/script.js
+++ Pari e Dispari/assets/script.js
@@ -1,7 +1,33 @@
 var scelta_Utente = prompt('Cosa preferisci Pari o Dispari')
+console.log('Hai scelto: '+ scelta_Utente)
 
 var n_Utente = parseInt(prompt('Inserisci un numero dal 1 al 5:'))
 console.log('Questo è il tuo numero:'+ n_Utente)
 
-var pc = Math.floor((Math.random() * 5) + 1);;
+var pc_rdm = pc();
 
+function pc(){
+  return  Math.ceil(Math.random() * 5);
+}
+
+function risultato(a,b){
+  return a + b;
+}
+
+console.log('La somma di entrambi numeri è: '+ risultato(n_Utente, pc_rdm))
+
+
+
+function game(){
+  if(!risultato % 2){
+    return 'pari';
+  }else{
+    return 'dispari';
+  }
+}
+
+if(!scelta_Utente == game()){
+  console.log('Hai vinto')
+}else{
+  console.log('Hai perso')
+}
